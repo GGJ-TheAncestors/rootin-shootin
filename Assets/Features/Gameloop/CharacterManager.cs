b543d8eac5c39a0d24d5d053fb35ca4542079cac
robertoost <github@robertoo.st>
2023-02-04 20:40:17 +0100

    feat(char_manager): spawn characters and possess

---
+++ Assets/Features/Gameloop/CharacterManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> CharacterPrefabs = new();
+    [SerializeField]
+    private List<GameObject> PlayerCharacters = new();
+    [SerializeField]
+    private List<Transform> SpawnPositions = new();
+    [SerializeField]
+    private RoleManager Roles;
+    [SerializeField]
+    private ReferenceList Players;
+
+    public void InstantiateCharacters()
+    {
+        for (int i = 0; i < Players.objects.Count; i++)
+        {
+            // For each player, spawn the object that they're playing as this round in their spawn position.
+            RoleManager.Characters role = Roles.GetRole(i);
+            PlayerCharacters.Add(Instantiate(CharacterPrefabs[(int)role]));
+            PlayerCharacters[i].transform.position = SpawnPositions[i].position;
+
+            // Assign control of the gameobject.
+            HandleInput handleInput = Players.objects[i].GetComponent<HandleInput>();
+            handleInput.Posses(PlayerCharacters[i]);
+        }
+    }
+
+    public void ClearCharacters()
+    {
+        foreach(GameObject character in PlayerCharacters) {
+            Destroy(character);
+        }
+        PlayerCharacters.Clear();
+    }
+}
