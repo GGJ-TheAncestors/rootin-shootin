7d2d18d89ac542368a1e0410dda48914b42bde1c
Paul van der Laan <dehupie@hotmail.com>
2023-02-04 15:35:40 +0100

    Basic WIP UI added

---
+++ Assets/UI/Scripts/PlayerInfoPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerInfoPanel : MonoBehaviour
+{
+    [SerializeField] Image healthImage;
+    [SerializeField] Image ammoImage;
+    [SerializeField] TMP_Text nameText;
+    [SerializeField] TMP_Text scoreText;
+
+    public Health player {get; private set; }
+
+    public void Initialize(Health playerHealth, ProjectileShooter projectileShooter)
+    {
+        player = playerHealth;
+    }
+}
