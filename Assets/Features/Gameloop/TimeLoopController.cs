dc728d29a94d5a9e6407a6c5302394b0df59f5b4
robertoost <github@robertoo.st>
2023-02-04 14:17:37 +0100

    feat(timeloop): add countdown & round timers

---
+++ Assets/Features/Gameloop/TimeLoopController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLoopController : MonoBehaviour
+{
+    // Main timeloop controller.
+    // Manages the round time, and the countdown time.
+    //
+    public bool RoundActive { get; private set; } = false;
+
+    public int Players = 2; // TODO: Get an actual list of players
+
+    // Keep track of the current round.
+    [SerializeField]
+    private int RoundDuration = 60;
+    private float RoundTime = 0;
+    public int RoundTimeUI { get; private set; } = 0;
+
+    // Countdown vars.
+    private bool CountDownActive = false;
+    private int CountDownDuration = 3;
+    private float CountDownTime = 3; // Time to count down from
+    public int CountDownTimeUI { get; private set; } = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeTimers();
+
+        Debug.Log("Start countdown!");
+        Debug.Log("3...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Main time loop.
+        if (RoundActive)
+        {
+            RoundTime -= Time.deltaTime;
+            RoundTimeUI = Mathf.CeilToInt(RoundTime);
+
+            if (RoundTime <= 0)
+            {
+                RoundActive = false;
+                Debug.Log("Round ended!");
+            }
+        }
+
+        // A countdown and then we start.
+        //
+        if (CountDownActive)
+        {
+            CountDownTime -= Time.deltaTime;
+            int prevCountDownTimeUI = CountDownTimeUI;
+            CountDownTimeUI = Mathf.CeilToInt(CountDownTime);
+
+            if (prevCountDownTimeUI != CountDownTimeUI)
+            {
+                Debug.Log(CountDownTimeUI == 0 ? "GO" : CountDownTimeUI.ToString() + "...");
+            }
+
+            if (CountDownTime <= 0)
+            {
+                RoundActive = true;
+
+                // Immediately start keeping track of the time.
+                //
+                RoundTime += CountDownTime * -1;
+
+                // Reset the countdown
+                CountDownTime = CountDownDuration;
+                CountDownActive = false;
+            }
+        }
+    }
+
+    void InitializeTimers()
+    {
+        // Start the countdown to the first round.
+        CountDownTime = CountDownDuration;
+        CountDownTimeUI = CountDownDuration;
+        CountDownActive = true;
+
+        RoundTime = RoundDuration;
+        RoundTimeUI = RoundDuration;
+        RoundActive = false;
+    }
+
+}
