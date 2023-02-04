ee4c04b295e34c8bb999a99e8b825e8a7ea36a22
robertoost <github@robertoo.st>
2023-02-04 16:09:04 +0100

    feat(gameloop): timeloop & score

---
+++ Assets/Features/Gameloop/ScoreManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Manages scores for each player over all 4 rounds.
+    // Supplies methods to add points to players.
+    [SerializeField]
+    private List<int> playerScores = new List<int> { 0, 0, 0, 0 };
+
+    public Action<int, int> ScoreAction;
+
+    public void AddScore(int score, int playerID)
+    {
+        playerScores[playerID] = score;
+        ScoreAction?.Invoke(score, playerID);
+    }
+
+    public void ResetScores() 
+    {
+        for (int i = 0; i < playerScores.Count; i++)
+        {
+            playerScores[i] = 0;
+        }
+    }
+
+    public int GetWinningPlayerID()
+    {
+        int playerID = playerScores.IndexOf(playerScores.Max());
+        return (playerID);
+    }
+}
