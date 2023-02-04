2c0589c792f47e5d86f6ae308793bb6c58275479
Jesse van Leeuwen <jesse@emberglitch.com>
2023-02-04 22:03:58 +0100

    fix: Clean Input Manager on Game loop

--- Assets/Features/Input/FillControllerList.cs
+++ Assets/Features/Input/FillControllerList.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using System;
 
 public class FillControllerList : MonoBehaviour
 {
     [SerializeField]
     private ReferenceList list;
 
+    private bool destroy;
+
     void Start()
     {
+        DontDestroyOnLoad( this.gameObject );
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        
         list.Clear();
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        if( arg0.buildIndex == 0 && destroy == true )
+            Destroy( this.gameObject );
+
+        if( arg0.buildIndex != 0 )
+        {
+            destroy = true;
+            GetComponent<PlayerInputManager>().DisableJoining();
+        }
     }
 
     void OnDestroy()
