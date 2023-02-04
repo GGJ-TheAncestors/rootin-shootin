07d94695718623e0fbaad823b9c17f8af7d071e1
Jesse van Leeuwen <jesse@emberglitch.com>
2023-02-04 17:30:58 +0100

    feat: event on object added to list

--- Assets/Scripts/ReferenceList.cs
+++ Assets/Scripts/ReferenceList.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(menuName = "ReferenceList")]
 public class ReferenceList : ScriptableObject
 {
     public List<GameObject> objects;
 
+    public UnityEvent<GameObject> OnAdded;
+
     public void Clear()
     {
+        OnAdded.RemoveAllListeners();
         objects = new List<GameObject>();
     }
 
@@ -18,10 +22,12 @@ public class ReferenceList : ScriptableObject
             objects = new List<GameObject>();
 
         objects.Add( obj );
+        OnAdded.Invoke( obj );
     }
 
     public void Remove( GameObject obj )
     {
         objects.Remove( obj );
+        
     }
 }
